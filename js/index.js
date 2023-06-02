e3afff7cc34c9c25f84c130611f481f00d3b3319
ecomeel <ecomeel@gmail.com>
2023-06-02 18:03:04 +0300

    done

--- js/index.js
+++ js/index.js
@@ -3,7 +3,10 @@ const ERROR_EMPTY_TEXTAREA = 'Empty textarea!';
 const ERROR_LARGE_TITLE = 'The title is more than 100 characters';
 const ERROR_LARGE_TEXTAREA = 'The text is more than 200 characters';
 const ERROR_CLASSNAME = 'error';
+const ERROR_CLASSNAME_COLOR = 'red-color';
 const EMPTY_POSTLIST_MESSAGE = 'Empty';
+const TITLE_VALIDATION_LIMIT = 100;
+const TEXTAREA_VALIDATION_LIMIT = 200;
 
 const posts = [];
 
@@ -14,36 +17,37 @@ const newPostBtnNode = document.getElementById('newPostBtn');
 const postsNode = document.getElementById('posts');
 
 const errorMessage = document.createElement('p');
-errorMessage.className = 'red-color'
+errorMessage.className = ERROR_CLASSNAME_COLOR;
 
 const getPostFromUser = () => {
     const time = new Date().toLocaleString().slice(0, -3);
     const title = newPostTitleInputNode.value;
     const text = newPostTextNode.value;
 
-    if (newPostTitleInputNode.value === '') {
+    if (newPostTitleInputNode.value.length === 0) {
         errorMessage.innerText = ERROR_EMPTY_TITLE;
         newPostTitleInputNode.classList.add(ERROR_CLASSNAME);
         newPostNode.appendChild(errorMessage);
         return;
-    } else if (newPostTitleInputNode.value.length > 100) {
+    } else if (newPostTitleInputNode.value.length > TITLE_VALIDATION_LIMIT) {
         errorMessage.innerText = ERROR_LARGE_TITLE;
         newPostTitleInputNode.classList.add(ERROR_CLASSNAME);
         newPostNode.appendChild(errorMessage);
         return;
-    } else if (newPostTextNode.value === '') {
+    } else if (newPostTextNode.value.length === 0) {
         newPostTitleInputNode.classList.remove(ERROR_CLASSNAME);
         newPostTextNode.classList.add(ERROR_CLASSNAME);
         errorMessage.innerText = ERROR_EMPTY_TEXTAREA;
         newPostNode.appendChild(errorMessage);
         return;
-    } else if (newPostTextNode.value.length > 200) {
+    } else if (newPostTextNode.value.length > TEXTAREA_VALIDATION_LIMIT) {
         errorMessage.innerText = ERROR_LARGE_TEXTAREA;
         newPostTextNode.classList.add(ERROR_CLASSNAME);
         newPostNode.appendChild(errorMessage);
         return;
     }
     newPostTextNode.classList.remove(ERROR_CLASSNAME);
+    newPostTitleInputNode.classList.remove(ERROR_CLASSNAME);
     errorMessage.innerText = '';
 
     return {
@@ -52,6 +56,7 @@ const getPostFromUser = () => {
         text
     }
 }
+
 
 const renderPosts = () => {
     if (postsNode.innerText == EMPTY_POSTLIST_MESSAGE) postsNode.innerText = '';
@@ -94,3 +99,4 @@ const newPostHandler = () => {
 }
 
 newPostBtnNode.addEventListener('click', newPostHandler);
+
